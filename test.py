518a3ff3a72db39ff2422c0755a41ae9c68b4286
JacktheGEAT <jacklynch706@gmail.com>
2026-01-20 02:03:26 -0500

    removed unnecessary files

--- test.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-from logger import logger as log
-# Load .env file if it exists
-load_dotenv()
-
-log.info(os.getenv("USER_NAME"))
-
-log.debug("This is a debug statement")
